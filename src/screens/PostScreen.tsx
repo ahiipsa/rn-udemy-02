a8e3afdfc0bdf93e57addc79895628f78c2d8c7a
Karasev Sergey <ahiipsa@gmail.com>
2020-02-27 01:14:43 +0300

    71. Add styles to PostScreen

--- src/screens/PostScreen.tsx
+++ src/screens/PostScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Alert, Button, Image, ScrollView, StyleSheet, Text, View} from 'react-native';
 import {NavigationStackScreenComponent} from 'react-navigation-stack';
+import {DATA} from '../data';
 import {THEME} from '../theme';
 
 type Params = {
@@ -12,10 +13,32 @@ type Props = {
 };
 export const PostScreen: NavigationStackScreenComponent<Params, Props> = ({navigation}) => {
   const postId = navigation.getParam('postId');
+  const post = DATA.find((item) => item.id === postId);
+
+  if (!post) {
+    return <View>Post not found {postId}</View>;
+  }
+
+  const handleRemove = () => {
+    Alert.alert(
+      'Delete a post',
+      'Are you sure?',
+      [
+        {text: 'Cancel', style: 'cancel'},
+        {text: 'Delete', style: 'destructive', onPress: () => console.log('OK Pressed')},
+      ],
+      {cancelable: false},
+    );
+  };
+
   return (
-    <View>
-      <Text>PostScreen {postId}</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{uri: post.img}} />
+      <View style={styles.titleWrap}>
+        <Text style={styles.title}>{post.text.repeat(100)}</Text>
+      </View>
+      <Button title="Delete" color={THEME.COLORS_DANGER} onPress={handleRemove} />
+    </ScrollView>
   );
 };
 
@@ -27,4 +50,15 @@ PostScreen.navigationOptions = ({navigation}) => {
   }
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  titleWrap: {
+    padding: 10,
+  },
+  title: {
+    fontFamily: 'opensans-regular',
+  }
+});
