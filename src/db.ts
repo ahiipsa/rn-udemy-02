6f44bdf87c7a8fd6be0c9442258a8ed9e5f76048
Karasev Sergey <ahiipsa@gmail.com>
2020-02-29 19:47:41 +0300

    94. Added filesystem

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,6 @@
 import * as SQLite from 'expo-sqlite';
+import * as FileSystem from 'expo-file-system';
+import {TPost} from './types';
 
 const db = SQLite.openDatabase('post.db');
 
@@ -9,7 +11,7 @@ export class DB {
     return new Promise((resolve, reject) => {
       db.transaction(tx => {
         tx.executeSql(
-          'CREATE TABLE IF NOT EXISTS post (id INTEGER PRIMARY KEY NOT NULL, text TEXT NOT NULL, img TEXT, booked INTEGER DEFAULT 0)',
+          'CREATE TABLE IF NOT EXISTS post (id INTEGER PRIMARY KEY NOT NULL, date TEXT, text TEXT NOT NULL, img TEXT, booked INTEGER DEFAULT 0)',
           [],
           () => {
             console.log('### database started...');
@@ -17,6 +19,52 @@ export class DB {
           },
           (_, error) => {
             console.log('### database error....', error);
+
+            reject(error);
+            return false;
+          }
+        );
+      });
+    });
+  }
+
+  static getPostList() {
+    return new Promise((resolve, reject) => {
+      db.transaction(tx => {
+        tx.executeSql(
+          'SELECT * FROM post;',
+          [],
+          (_, result) => {
+            resolve(result.rows._array);
+            return [];
+          },
+          (_, error) => {
+            reject(error);
+            return false;
+          }
+        );
+      });
+    });
+  }
+
+  static createPost(post: {
+    text: string;
+    date: string;
+    booked: boolean;
+    img: string;
+  }) {
+    return new Promise((resolve, reject) => {
+      db.transaction(tx => {
+        tx.executeSql(
+          `INSERT INTO post (text, img, booked, date) VALUES (?, ?, ?, ?)`,
+          [post.text, post.img, 0, post.date],
+          (_, result) => {
+            console.log('### post Created', result);
+
+            resolve(result.insertId);
+          },
+          (_, error) => {
+            console.log('### post create error', error, _);
 
             reject(error);
             return false;
--- src/store/actions/post.ts
+++ src/store/actions/post.ts
@@ -1,35 +1,67 @@
-import {POST_CREATE, POST_LOAD_LIST, POST_REMOVE, POST_TOGGLE_BOOKED} from '../types';
+import * as FileSystem from 'expo-file-system';
+import {DB} from '../../db';
+import {
+  POST_CREATE,
+  POST_LOAD_LIST,
+  POST_REMOVE,
+  POST_TOGGLE_BOOKED,
+} from '../types';
 
-export const loadPostList = () => {
-  return {
+export const loadPostList = () => async dispatch => {
+  const postList = await DB.getPostList();
+
+  return dispatch({
     type: POST_LOAD_LIST,
-    payload: [],
-  }
+    payload: postList,
+  });
 };
 
-export const toggleBooked = (postId) => {
+export const toggleBooked = postId => {
   return {
     type: POST_TOGGLE_BOOKED,
     payload: postId,
-  }
+  };
 };
 
-export const removePost = (postId)  => {
+export const removePost = postId => {
   return {
     type: POST_REMOVE,
     payload: postId,
-  }
+  };
 };
 
-export const createPost = ({text, img}) => {
-  return {
-    type: POST_CREATE,
-    payload: {
-      id: Date.now().toString(),
-      text,
-      img,
-      date: new Date().toJSON(),
-      booked: false,
-    }
+export const createPost = ({text, img}) => async dispatch => {
+  const fileName = img.split('/').pop();
+  const filePath = FileSystem.documentDirectory + fileName;
+
+  try {
+    await FileSystem.moveAsync({
+      to: filePath,
+      from: img,
+    });
+  } catch (e) {
+    console.log('### error', e);
+    return;
   }
-}
+
+  const post = {
+    text,
+    img: filePath,
+    date: new Date().toJSON(),
+    booked: false,
+  };
+
+  console.log('### post', post);
+
+  const postId = await DB.createPost(post);
+
+  const payload = {
+    ...post,
+    id: postId,
+  };
+
+  return dispatch({
+    type: POST_CREATE,
+    payload,
+  });
+};
